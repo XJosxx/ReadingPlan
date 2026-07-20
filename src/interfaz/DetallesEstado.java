27071c4a78a57f69d2ff5403174eef994a53773b
XJosxx <josx130@gmail.com>
2026-07-20 16:34:39 -0500

    fix: correccion de bugs para el funcionamiento de un Libro

--- src/interfaz/DetallesEstado.java
+++ src/interfaz/DetallesEstado.java
@@ -1,4 +1,11 @@
 package interfaz;
 
 public interface DetallesEstado {
+    default String obtenerTipo() {
+        return null;
+    } // retorna "LEYENDO", "LEIDO", "PLANEADO"
+
+    default String obtenerResumen() {
+        return null;
+    } // texto corto para mostrar el tabla
 }
